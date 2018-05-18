f9617003eb0df534b221a0e894c19e00c1a35aa9
xinghan <xinghan@ebay.com>
2018-05-18 08:55:18 +0800

    init commit

---
+++ src/main/java/com/xingh3/springDemo/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.xingh3.springDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GreetingController {
+	
+	@RequestMapping("/")
+	public String greeting() {
+		return "index.html";
+	}
+}
